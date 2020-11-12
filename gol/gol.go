05cea3b4e86da615fda3c62b134f5640c4aa3ee7
Chris Gora <34940205+ChrisGora@users.noreply.github.com>
2020-11-12 15:51:52 +0000

    Added skeleton

---
+++ gol/gol.go
@@ -0,0 +1,32 @@
+package gol
+
+// Params provides the details of how to run the Game of Life and which image to load.
+type Params struct {
+	Turns       int
+	Threads     int
+	ImageWidth  int
+	ImageHeight int
+}
+
+// Run starts the processing of Game of Life. It should initialise channels and goroutines.
+func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
+
+	ioCommand := make(chan ioCommand)
+	ioIdle := make(chan bool)
+
+	distributorChannels := distributorChannels{
+		events,
+		ioCommand,
+		ioIdle,
+	}
+	go distributor(p, distributorChannels)
+
+	ioChannels := ioChannels{
+		command:  ioCommand,
+		idle:     ioIdle,
+		filename: nil,
+		output:   nil,
+		input:    nil,
+	}
+	go startIo(p, ioChannels)
+}
